b5c5b9d78686c626a603a8e7a3f1c0af7e25c272
Kongesque <Kongesque@gmail.com>
2024-12-22 02:59:40 +0800

    Update UI layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@
 import "../app/globals.css";
 import dynamic from 'next/dynamic';
 import Image from "next/image";
-import { Github, Send, MapPin } from 'lucide-react';
+import { Github, Send, MapPin, Landmark } from 'lucide-react';
 
 // import SnakeGame from "@/components/snake";
 
@@ -17,7 +17,7 @@ export function Header() {
             <div className="px-4">
                 <div className="flex justify-between items-start">
                     <div className="relative -mt-20 mb-4">
-                        <div className="w-32 h-32 rounded-xl border-[4px] border-background overflow-hidden">
+                        <div className="w-32 h-32 rounded-xl border-[5px] border-background overflow-hidden">
                             <Image
                                 src="/kongesque.jpg"
                                 alt="Profile photo"
@@ -33,13 +33,13 @@ export function Header() {
                             href="https://github.com/Kongesque" 
                             target="_blank" 
                             rel="noopener noreferrer" 
-                            className="flex items-center justify-center w-9 h-9 border-[1px] border-blockBorder text-secondary rounded hover:text-primary transition duration-300"
+                            className="flex items-center justify-center w-9 h-9 border-[1px] border-blockBorder text-secondary rounded hover:text-primary transition-colors duration-200"
                         >
                             <Github className="w-4 h-4" />
                         </a>
                         <a 
                             href="mailto:kongesque@gmail.com" 
-                            className="w-9 h-9 flex items-center justify-center bg-accent text-background rounded hover:bg-accentHover transition duration-300 text-sm sm:px-4 sm:py-2 sm:w-auto sm:h-auto"
+                            className="w-9 h-9 flex items-center justify-center bg-accent text-background rounded hover:bg-accentHover transition-colors duration-200 text-sm sm:px-4 sm:py-2 sm:w-auto sm:h-auto"
                         >
                             <span className="hidden sm:inline">Contact</span>
                             <Send className="w-4 h-4 sm:hidden" />
@@ -57,9 +57,15 @@ export function Header() {
                 is a creative problem solver who tackles complex challenges and games by designing solutions through proven AI algorithms, Deep Reinforcement Learning, and Computer Games Theory.
                 </p>
 
-                <div className="flex items-center gap-2 text-secondary text-sm">
-                    <MapPin className="w-4 h-4" />
-                    everywhere
+                <div className="flex items-center gap-4 text-secondary text-sm">
+                    <div className="flex items-center gap-2">
+                        <Landmark className="w-4 h-4" />
+                        Computer Science @NDHU
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <MapPin className="w-4 h-4" />
+                        Taiwan
+                    </div>
                 </div>
             </div>
         </header>

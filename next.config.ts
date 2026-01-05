853d8d6b0590042fe957094df780f0a5053d14de
Kongesque <Kongesque@gmail.com>
2026-01-05 22:42:13 +0800

    feat: replace 'Introducing ZoneNet' post with 'Introducing Locus'

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['www.kongesque.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/blog/introducing-zonenet',
+        destination: '/blog/introducing-locus',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
